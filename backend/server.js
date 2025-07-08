435c09087b4d009142c36d118f08e6dad1832fb9
Suhana Gupta <guptasuhana873@gmail.com>
2025-07-08 16:35:09 +0530

    project completed

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ import couponRoutes from './routes/couponRoutes.js'
 import paymentRoutes from './routes/paymentRoutes.js'
 import analysisRoutes from './routes/analyticsRoutes.js'
 import cors from 'cors';
-import path from 'path'
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
@@ -17,7 +22,6 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-const _dirname = path.resolve();
 
 const corsOptions = {
     origin: 'http://localhost:5173', // Allow only requests from this origin
@@ -39,13 +43,12 @@ app.use("/api/analytics", analysisRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(_dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-    app.get("/*", (req, res) => {
-        res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
